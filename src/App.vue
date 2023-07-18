60f1504f8607b87e15aeba57d55c4158a6200e79
zerin22 <tajrin.022@gmail.com>
2023-07-18 22:11:44 +0600

    upload

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+        <div class="container">
+            <div class="mobile-container">
+                <!-- Top Navigation Menu -->
+                <div class="topnav d-flex">
+                    <a href="index.html" class="active">FitAdvice</a>
+                    <div id="myLinks">
+                        <a href="index.html">Home</a>
+                    </div>
+                    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                        <i class="fa fa-bars"></i>
+                    </a>
+                </div>
+                <div class="nav_boder"></div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <banner>
+            <div class="container">
+                <div class="banner_img">
+                    <img class="img-fluid" src="/images/banner.png" alt="">
+                </div>
+                <div class="banner_inner text-center bottom_gap">
+                    <h1>Discover the Ultimate Mobile Experience</h1>
+                    <p>
+                        Effortlessly Connect, Engage, and Stay Up-to-Date with Our Intuitive App Interface.
+                    </p>
+                    <button>Download the app</button>
+                </div>
+            </div>
+        </banner>
+
+        <section>
+            <div class="container">
+                <div class="about_us bottom_gap d-flex justify-content-between align-items-center">
+                    <img class="img-fluid" src="/images/iPhone 12 Pro.png" alt="">
+                    <div class="text-center banner_inner">
+                        <h2>About Us</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="about_us text-center bottom_gap">
+                    <h2>Features</h2>
+                    <p>Dictumst feugiat eget at et quam. Nulla aliquam bibendum amet faucibus. Scelerisque ac dolor facilisis justo scelerisque.</p>
+                </div>
+                <div class="row gap-2">
+                    <div class="col-12">
+                        <div class="freature_inner text-center">
+                            <img src="/images/feature_1.png" alt="">
+                            <h5>Fully Secured</h5>
+                            <p>Dictumst feugiat eget at et quam. Nulla aliquam bibendum amet faucibus. Scelerisque ac dolor facilisis justo scelerisque.</p>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="freature_inner text-center">
+                            <img src="/images/feature_2.png" alt="">
+                            <h5>Easy To Find</h5>
+                            <p>Dictumst feugiat eget at et quam. Nulla aliquam bibendum amet faucibus. Scelerisque ac dolor facilisis justo scelerisque.</p>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="freature_inner text-center">
+                            <img src="/images/feature_3.png" alt="">
+                            <h5>24/7 Service</h5>
+                            <p>Dictumst feugiat eget at et quam. Nulla aliquam bibendum amet faucibus. Scelerisque ac dolor facilisis justo scelerisque.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="about_us text-center bottom_gap">
+                    <h2>App Screens</h2>
+                    <div class="app_screens_inner">
+                        <img class="img-fluid" src="/images/app_screens.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="about_us text-center bottom_gap">
+                <h2>Our Happy Clients</h2>
+                <h6>Says About Us</h6>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet</p>
+                <div class="happy_client">
+                    <img class="img-fluid" src="/images/happy.png" alt="">
+                    <h4>Johney Smith</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="d-flex justify-content-between align-items-center gap-5 text-start">
+            <div>
+                <h3>Logo</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur. </p>
+            </div>
+            <div>
+                <div class="d-flex align-items-start">
+                    <img class="foo_img" src="/images/location.png" alt="">
+                    <h3>Address</h3>
+                </div>
+                <p>Montagne de Saint-Job 39, Brussels</p>
+                <p>+02 375 30 53</p>
+                <p>company@gmail.com</p>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
